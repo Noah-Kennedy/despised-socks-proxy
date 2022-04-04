5514dc92af2b32970b599fbf917ff9792ad420e9
noah <nomaxx117@gmail.com>
2022-04-03 21:10:44 -0500

    socks

---
+++ despicable-proxy-cli/src/main.rs
@@ -0,0 +1,36 @@
+use std::net::{IpAddr, SocketAddr};
+use despicable_socks5::server::{Socks5Server, Socks5ServerConfig};
+
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    /// IP address to serve on
+    #[clap(long, group = "socks")]
+    socks_ip: Option<IpAddr>,
+    /// Port to serve on
+    #[clap(long, requires = "socks")]
+    socks_port: Option<u16>,
+}
+
+#[tokio::main(flavor = "current_thread")]
+async fn main() -> anyhow::Result<()> {
+    tracing_subscriber::fmt::fmt().init();
+
+    let args = Args::parse();
+
+    if let Args { socks_ip: Some(ip), socks_port: Some(port), ..} = args {
+        tracing::info!("Launching socks proxy");
+
+        let config = Socks5ServerConfig {
+            addr: SocketAddr::new(ip, port),
+        };
+
+        let server = Socks5Server::launch(config);
+
+        server.acceptor.await.unwrap();
+    }
+
+    Ok(())
+}
---
+++ despicable-socks5/src/messages.rs
@@ -0,0 +1,214 @@
+pub const AUTH_METHODS_NO_AUTHENTICATION_REQUIRED: u8 = 0x00;
+pub const AUTH_METHODS_GSSAPI: u8 = 0x01;
+pub const AUTH_METHODS_USERNAME_PASSWORD: u8 = 0x02;
+pub const AUTH_METHODS_NO_ACCEPTABLE_METHODS: u8 = 0xFF;
+
+pub const CMD_CONNECT: u8 = 0x01;
+pub const CMD_BIND: u8 = 0x02;
+pub const CMD_UDP_ASSOCIATE: u8 = 0x03;
+
+pub const STATUS_SUCCESS: u8 = 0x00;
+pub const STATUS_SOCKS_FAIL: u8 = 0x01;
+pub const STATUS_NOT_ALLOWED: u8 = 0x02;
+pub const STATUS_NET_UNREACHABLE: u8 = 0x03;
+pub const STATUS_HOST_UNREACHABLE: u8 = 0x04;
+pub const STATUS_CONN_REFUSED: u8 = 0x05;
+pub const STATUS_TTL_EXP: u8 = 0x06;
+pub const STATUS_COMMAND_UNSUPPORTED: u8 = 0x07;
+pub const STATUS_ADDRESS_UNSUPPORTED: u8 = 0x08;
+
+pub const ATYP_V4: u8 = 0x01;
+pub const ATYP_DOMAIN: u8 = 0x03;
+pub const ATYP_V6: u8 = 0x04;
+
+#[derive(Copy, Clone, Ord, PartialOrd, Eq, PartialEq, Debug, Default, Hash)]
+pub struct Greeting<B>(pub B);
+
+#[derive(Copy, Clone, Ord, PartialOrd, Eq, PartialEq, Debug, Default, Hash)]
+pub struct ServerChoice<B>(pub B);
+
+#[derive(Copy, Clone, Ord, PartialOrd, Eq, PartialEq, Debug, Default, Hash)]
+pub struct UsernamePasswordAuthRequest<B>(pub B);
+
+#[derive(Copy, Clone, Ord, PartialOrd, Eq, PartialEq, Debug, Default, Hash)]
+pub struct Status<B>(pub B);
+
+#[derive(Copy, Clone, Ord, PartialOrd, Eq, PartialEq, Debug, Default, Hash)]
+pub struct Connection<B>(pub B);
+
+impl<B> Greeting<B>
+where
+    B: AsRef<[u8]>,
+{
+    #[inline]
+    pub fn verify(&self) -> bool {
+        let slice = self.0.as_ref();
+        slice.len() >= 2 && slice.len() - 2 == slice[1] as usize
+    }
+    #[inline]
+    pub fn version(&self) -> &u8 {
+        &self.0.as_ref()[0]
+    }
+    #[inline]
+    pub fn nmethods(&self) -> &u8 {
+        &self.0.as_ref()[1]
+    }
+    #[inline]
+    pub fn methods(&self) -> &[u8] {
+        &self.0.as_ref()[2..]
+    }
+    #[inline]
+    pub fn filter_for_method(&self, target: u8) -> bool {
+        self.methods().iter().any(|b| *b == target)
+    }
+}
+
+impl<B> ServerChoice<B>
+where
+    B: AsRef<[u8]>,
+{
+    #[inline]
+    pub fn version(&self) -> &u8 {
+        &self.0.as_ref()[0]
+    }
+    #[inline]
+    pub fn method(&self) -> &u8 {
+        &self.0.as_ref()[1]
+    }
+}
+
+impl<B> ServerChoice<B>
+where
+    B: AsMut<[u8]>,
+{
+    #[inline]
+    pub fn version_mut(&mut self) -> &mut u8 {
+        &mut self.0.as_mut()[0]
+    }
+    #[inline]
+    pub fn method_mut(&mut self) -> &mut u8 {
+        &mut self.0.as_mut()[1]
+    }
+}
+
+impl<B> UsernamePasswordAuthRequest<B>
+where
+    B: AsRef<[u8]>,
+{
+    #[inline]
+    pub fn version(&self) -> &u8 {
+        &self.0.as_ref()[0]
+    }
+    #[inline]
+    pub fn ulen(&self) -> &u8 {
+        &self.0.as_ref()[1]
+    }
+    #[inline]
+    pub fn uname(&self) -> &[u8] {
+        &self.0.as_ref()[2..2 + *self.ulen() as usize]
+    }
+    #[inline]
+    pub fn plen(&self) -> &u8 {
+        &self.0.as_ref()[2 + *self.ulen() as usize]
+    }
+    #[inline]
+    pub fn password(&self) -> &[u8] {
+        let offset = 3 + *self.ulen() as usize;
+        &self.0.as_ref()[3 + *self.ulen() as usize..offset + *self.plen() as usize]
+    }
+}
+
+impl<B> Status<B>
+where
+    B: AsRef<[u8]>,
+{
+    #[inline]
+    pub fn version(&self) -> &u8 {
+        &self.0.as_ref()[0]
+    }
+    #[inline]
+    pub fn status(&self) -> &u8 {
+        &self.0.as_ref()[1]
+    }
+}
+
+impl<B> Status<B>
+where
+    B: AsMut<[u8]>,
+{
+    #[inline]
+    pub fn version_mut(&mut self) -> &mut u8 {
+        &mut self.0.as_mut()[0]
+    }
+    #[inline]
+    pub fn status_mut(&mut self) -> &mut u8 {
+        &mut self.0.as_mut()[1]
+    }
+}
+
+impl<B> Connection<B>
+where
+    B: AsRef<[u8]>,
+{
+    #[inline]
+    pub fn version(&self) -> &u8 {
+        &self.0.as_ref()[0]
+    }
+    #[inline]
+    pub fn cmd(&self) -> &u8 {
+        &self.0.as_ref()[1]
+    }
+    #[inline]
+    pub fn rsv(&self) -> &u8 {
+        &self.0.as_ref()[2]
+    }
+    #[inline]
+    pub fn atyp(&self) -> &u8 {
+        &self.0.as_ref()[3]
+    }
+    #[inline]
+    pub fn addr(&self) -> &[u8] {
+        let buf = self.0.as_ref();
+        &buf[4..(buf.len() - 2)]
+    }
+    #[inline]
+    pub fn port(&self) -> u16 {
+        let buf = self.0.as_ref();
+        u16::from_be_bytes([buf[buf.len() - 2], buf[buf.len() - 1]])
+    }
+}
+
+impl<B> Connection<B>
+where
+    B: AsMut<[u8]>,
+{
+    #[inline]
+    pub fn version_mut(&mut self) -> &mut u8 {
+        &mut self.0.as_mut()[0]
+    }
+    #[inline]
+    pub fn status_mut(&mut self) -> &mut u8 {
+        &mut self.0.as_mut()[1]
+    }
+    #[inline]
+    pub fn rsv_mut(&mut self) -> &mut u8 {
+        &mut self.0.as_mut()[2]
+    }
+    #[inline]
+    pub fn atyp_mut(&mut self) -> &mut u8 {
+        &mut self.0.as_mut()[3]
+    }
+    #[inline]
+    pub fn addr_mut(&mut self) -> &mut [u8] {
+        let buf = self.0.as_mut();
+        let len = buf.len();
+        &mut buf[4..(len - 2)]
+    }
+    #[inline]
+    pub fn set_port(&mut self, port: u16) {
+        let len = self.0.as_mut().len();
+        let bytes = port.to_be_bytes();
+        self.0.as_mut()[len - 2] = bytes[0];
+        self.0.as_mut()[len - 1] = bytes[1];
+    }
+}
